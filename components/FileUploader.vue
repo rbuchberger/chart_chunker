443a280d1135dd52c6d5828f27dcd839012a9915
Robert Buchberger <robert@buchberger.cc>
2019-09-26 14:44:37 +0200

    Add functioning text file uploader.

---
+++ components/FileUploader.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <input type="file" @change="loadText" /> -->
+  <v-file-input @change="loadText" />
+</template>
+
+<script>
+export default {
+  methods: {
+    loadText(file) {
+      const reader = new FileReader()
+
+      reader.onload = (e) => this.$emit('load', e.target.result)
+      reader.readAsText(file)
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,48 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+      <header class="text-center">
+        <h1>Battery Chart splitter</h1>
+        <p>So you don't have to abuse undergrads</p>
+      </header>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          First, upload your file.
         </v-card-title>
         <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
+          <p>Just give me whatever the tester spit out.</p>
+          <!-- <v-file-input v-model="inputFile" /> -->
+          <FileUploader @load="text = $event" />
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
+        <!-- <v-card-actions>                                           -->
+        <!--   <v-spacer />                                             -->
+        <!--   <v-btn color="orange" @click="loadContents">Next</v-btn> -->
+        <!-- </v-card-actions>                                          -->
       </v-card>
+      {{ text }}
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import FileUploader from '~/components/FileUploader'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    FileUploader
+  },
+  data: () => ({ text: '' }),
+
+  computed: {},
+
+  methods: {
+    loadContents(e) {
+      const reader = new FileReader()
+      const file = e.target.files[0]
+
+      reader.onload = (em) => console.log(em.target.result)
+      reader.readAsText(file)
+    }
   }
 }
 </script>
