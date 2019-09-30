0cc81411af21eabb6b018e5e2675b772fcf05337
Robert Buchberger <robert@buchberger.cc>
2019-09-30 17:01:34 +0200

    Add copy to clipboard functionality

--- components/DataOutput.vue
+++ components/DataOutput.vue
@@ -1,53 +1,55 @@
 <template>
   <v-card class="my-5">
-    <v-card-title class="headline">
-      Preview Cycle # {{ selectedCycleIndex + 1 }} of {{ cycleCount }}
+    <v-card-title>
+      Cycle # {{ selectedCycleIndex + 1 }} of {{ cycleCount }}
     </v-card-title>
     <v-card-text>
-      <v-switch label="Preview" v-model="preview" color="orange" />
-      <div v-if="preview">
-        <v-slider
-          v-model="selectedCycleIndex"
-          :max="cycleCount - 1"
-          v-color="orange"
-          ticks
-        />
-        <v-simple-table dense light>
-          <thead>
-            <tr>
-              <th v-for="header in selectedCycle.headers" :key="header">
-                {{ header }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(line, lineIndex) in selectedCycle.data"
-              :key="lineIndex"
+      <v-slider
+        v-model="selectedCycleIndex"
+        :max="cycleCount - 1"
+        color="orange"
+        label="Select Cycle: "
+        ticks
+      />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn @click="copyCurrent">Copy current</v-btn>
+        <v-btn @click="copyAll">Copy all</v-btn>
+        <v-btn color="orange">Download all</v-btn>
+        <v-spacer />
+      </v-card-actions>
+      <textarea hidden="true" id="select-box" />
+      <v-simple-table dense light>
+        <thead>
+          <tr>
+            <th v-for="header in selectedCycle.headers" :key="header">
+              {{ header }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(line, lineIndex) in selectedCycle.data" :key="lineIndex">
+            <td
+              v-for="(entry, entryIndex) in line"
+              :key="lineIndex.toString() + entryIndex.toString()"
             >
-              <td
-                v-for="(entry, entryIndex) in line"
-                :key="lineIndex.toString() + entryIndex.toString()"
-              >
-                {{ entry }}
-              </td>
-            </tr>
-          </tbody>
-        </v-simple-table>
-      </div>
+              {{ entry }}
+            </td>
+          </tr>
+        </tbody>
+      </v-simple-table>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Papa from 'papaparse'
 
 export default {
   data() {
     return {
-      selectedCycleIndex: 0,
-      preview: true,
-      preview_graph: false
+      selectedCycleIndex: 0
     }
   },
   computed: {
@@ -55,6 +57,45 @@ export default {
 
     selectedCycle() {
       return this.cycles[this.selectedCycleIndex]
+    },
+
+    unparsedCurrent() {
+      const matrix = [this.selectedCycle.headers].concat(
+        this.selectedCycle.data
+      )
+      return Papa.unparse(matrix)
+    },
+
+    unparsedAll() {
+      const matrix = [[]]
+      this.cycles.forEach((cycle) => {
+        matrix[0].push(...cycle.headers)
+        cycle.data.forEach((line, lineNumber) => {
+          matrix[lineNumber + 1] = matrix[lineNumber + 1] || []
+
+          matrix[lineNumber + 1].push(...line)
+        })
+      })
+
+      return Papa.unparse(matrix)
+    }
+  },
+
+  methods: {
+    successfulCopy() {
+      console.log('SuccessfulCopy')
+    },
+
+    errorCopy() {
+      console.log('Error copying text')
+    },
+
+    copyAll() {
+      this.$copyText(this.unparsedAll)
+    },
+
+    copyCurrent() {
+      this.$copyText(this.unparsedCurrent)
     }
   }
 }
