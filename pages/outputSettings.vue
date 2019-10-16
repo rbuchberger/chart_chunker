08592c5f0082ac1a0e8d2233b9b8d9cd4e341ba9
Robert Buchberger <robert@buchberger.cc>
2019-10-16 14:41:44 +0200

    Build UI to input new settings

--- pages/outputSettings.vue
+++ pages/outputSettings.vue
@@ -6,33 +6,63 @@
       <v-btn @click="handleNext" :loading="loading" color="orange">Next</v-btn>
     </v-card-actions>
     <v-card-text>
-      <h2 class="mt-5">Split Basis:</h2>
-      <p>
-        Which value should I use to sort one cycle from the next?
-      </p>
-      <v-select
-        :items="columnItems"
-        :value="splitBasis"
-        @change="setSplitBasis"
-        hint="I'll split where it changes sign, and discard lines where it's ~zero."
-        persistent-hint
-      />
+      <h2 class="mt-5 text-center">Which columns are which?</h2>
     </v-card-text>
-    <hr class="my-5" />
-    <v-card-text>
-      <h2 class="mt-5">Select columns to keep:</h2>
-      <p class="caption">All data will be returned as absolute values.</p>
-      <v-row justify="start">
-        <v-checkbox
-          v-for="(column, index) in columns"
-          :key="index"
-          v-model="handleToggle"
-          :value="index"
-          :label="column"
-          color="orange"
-          class="mx-3"
-        />
+    <v-container class="pb-5">
+      <v-row>
+        <v-col>
+          <v-select
+            :items="columnItems"
+            :value="splitBasis"
+            @change="setSplitBasis"
+            label="Charge Current"
+            hint="I'll use this value's sign to sort out cycles, and discard
+            lines where it's ~zero. It doesn't have to be included in the output
+            data."
+            persistent-hint
+          />
+        </v-col>
+        <v-col>
+          <v-select
+            :items="keptColumnItems"
+            :value="voltageColumn"
+            @change="setVoltageColumn"
+            label="Voltage"
+          />
+        </v-col>
+        <v-col>
+          <v-select
+            :items="keptColumnItems"
+            :value="spcColumn"
+            @change="setSpcColumn"
+            label="Specific Capacity"
+          />
+        </v-col>
       </v-row>
+    </v-container>
+    <v-card-text>
+      <h2 class="mt-5 text-center">Select columns to keep.</h2>
+      <p class="caption text-center">
+        All data will be returned as absolute values.
+      </p>
+      <v-container>
+        <v-row no-gutters>
+          <v-col
+            cols="12"
+            md="3"
+            sm="4"
+            v-for="(column, index) in columns"
+            :key="index"
+          >
+            <v-checkbox
+              v-model="handleToggle"
+              :value="index"
+              :label="column"
+              color="orange"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-card-text>
   </v-card>
 </template>
@@ -42,7 +72,13 @@ import { mapGetters, mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
   computed: {
-    ...mapGetters(['columnItems', 'columns']),
+    ...mapGetters([
+      'columnItems',
+      'columns',
+      'keptColumnItems',
+      'spcColumn',
+      'voltageColumn'
+    ]),
     ...mapState(['splitBasis', 'keptColumns', 'loading']),
     handleToggle: {
       get() {
@@ -55,7 +91,13 @@ export default {
   },
 
   methods: {
-    ...mapMutations(['setSplitBasis', 'setKeptColumns']),
+    ...mapMutations([
+      'setSplitBasis',
+      'setKeptColumns',
+      'setSpcColumn',
+      'setVoltageColumn'
+    ]),
+
     ...mapActions(['buildChunker']),
     handleNext() {
       this.buildChunker().then(this.$router.push('dataOutput'))
