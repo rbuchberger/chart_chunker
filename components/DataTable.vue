e1f836c81b3ad3bf94b45bde52bcd045c40c1ca6
Robert Buchberger <robert@buchberger.cc>
2019-10-17 18:20:04 +0200

    Create datatable component

---
+++ components/DataTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-simple-table dense fixed-header height="800">
+    <thead>
+      <tr>
+        <th v-for="header in dataObject.headers" :key="header">{{ header }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(line, lineIndex) in dataObject.lines" :key="lineIndex">
+        <td
+          v-for="(entry, entryIndex) in line"
+          :key="lineIndex.toString() + entryIndex.toString()"
+        >
+          {{ entry }}
+        </td>
+      </tr>
+    </tbody>
+  </v-simple-table>
+</template>
+<script>
+export default {
+  props: ['dataObject']
+}
+</script>
