cb8c56a82d6c48ec7b66e1fc35d0b74d685a309e
Robert Buchberger <robert@buchberger.cc>
2019-11-08 18:33:27 +0100

    Add filename to output settings display

--- pages/outputSettings.vue
+++ pages/outputSettings.vue
@@ -1,18 +1,22 @@
 <template>
   <v-card>
     <v-card-actions>
-      <v-btn nuxt color="grey darken-2" to="/">
-        Back
-      </v-btn>
+      <v-btn nuxt color="grey darken-2" to="/">Back</v-btn>
       <v-spacer />
-      <v-btn :loading="loading" color="orange" @click="handleNext">
-        Next
-      </v-btn>
+      <v-btn
+        outlined
+        elevation="12"
+        color="orange"
+        class="mono body-2 nocap"
+        nuxt
+        to="/"
+        large
+      >{{ file.name }}</v-btn>
+      <v-spacer />
+      <v-btn :loading="loading" color="orange" @click="handleNext">Next</v-btn>
     </v-card-actions>
     <v-card-text>
-      <h2 class="mt-5 text-center">
-        Which columns are which?
-      </h2>
+      <h2 class="text-center">Which columns are which?</h2>
     </v-card-text>
     <v-container class="pb-5">
       <v-row>
@@ -47,27 +51,12 @@
       </v-row>
     </v-container>
     <v-card-text>
-      <h2 class="mt-5 text-center">
-        Select columns to keep.
-      </h2>
-      <p class="caption text-center">
-        All data will be returned as absolute values.
-      </p>
+      <h2 class="mt-5 text-center">Select columns to keep:</h2>
+      <p class="text-center">All data will be returned as absolute values.</p>
       <v-container>
         <v-row no-gutters>
-          <v-col
-            v-for="(column, index) in columns"
-            :key="index"
-            cols="12"
-            md="3"
-            sm="4"
-          >
-            <v-checkbox
-              v-model="handleToggle"
-              :value="index"
-              :label="column"
-              color="orange"
-            />
+          <v-col v-for="(column, index) in columns" :key="index" cols="12" md="3" sm="4">
+            <v-checkbox v-model="handleToggle" :value="index" :label="column" color="orange" />
           </v-col>
         </v-row>
       </v-container>
@@ -75,6 +64,15 @@
   </v-card>
 </template>
 
+<style>
+.nocap {
+  text-transform: none !important;
+}
+
+.mono {
+  font-family: mono;
+}
+</style>
 <script>
 import { mapGetters, mapState, mapMutations, mapActions } from 'vuex'
 
@@ -87,7 +85,7 @@ export default {
       'spcColumn',
       'voltageColumn'
     ]),
-    ...mapState(['splitBasis', 'keptColumns', 'loading']),
+    ...mapState(['splitBasis', 'keptColumns', 'loading', 'file']),
     handleToggle: {
       get() {
         return this.keptColumns
