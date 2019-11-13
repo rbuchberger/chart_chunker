7e15ecd8e9a1ba0ef67beb4dadca81a4d6d7df5f
Robert Buchberger <robert@buchberger.cc>
2019-11-13 14:55:51 +0100

    Condense data table view

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,24 +1,51 @@
 <template>
-  <v-simple-table dense fixed-header height="800">
-    <thead>
-      <tr>
-        <th v-for="header in dataObject.headers" :key="header">{{ header }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(line, lineIndex) in dataObject.lines" :key="lineIndex">
-        <td
-          v-for="(entry, entryIndex) in line"
-          :key="lineIndex.toString() + entryIndex.toString()"
+  <v-container>
+    <v-row>
+      <v-spacer />
+      <v-col sm="8">
+        <v-simple-table
+          dense
+          fixed-header
+          height="500"
+          class="datatable"
         >
-          {{ entry }}
-        </td>
-      </tr>
-    </tbody>
-  </v-simple-table>
+          <thead>
+            <tr>
+              <th
+                v-for="header in dataObject.headers"
+                :key="header"
+              >
+                {{ header }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(line, lineIndex) in dataObject.lines"
+              :key="lineIndex"
+            >
+              <td
+                v-for="(entry, entryIndex) in line"
+                :key="lineIndex.toString() + entryIndex.toString()"
+              >
+                {{ entry }}
+              </td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-col>
+      <v-spacer />
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
   props: ['dataObject']
 }
 </script>
+
+<style scoped>
+table.datatable {
+  table-layout: fixed;
+}
+</style>
