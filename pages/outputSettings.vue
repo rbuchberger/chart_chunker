6c3ee445b71fb6fb9c3d51ba205c1e5d5abab1c6
Robert Buchberger <robert@buchberger.cc>
2019-11-13 14:57:48 +0100

    Rebuild output settings page

--- pages/outputSettings.vue
+++ pages/outputSettings.vue
@@ -1,66 +1,160 @@
 <template>
   <v-card>
-    <v-card-actions>
-      <v-btn nuxt color="grey darken-2" to="/">Back</v-btn>
-      <v-spacer />
-      <v-btn
-        outlined
-        elevation="12"
-        color="orange"
-        class="mono body-2 nocap"
-        nuxt
-        to="/"
-        large
-      >{{ file.name }}</v-btn>
-      <v-spacer />
-      <v-btn :loading="loading" color="orange" @click="handleNext">Next</v-btn>
-    </v-card-actions>
-    <v-card-text>
-      <h2 class="text-center">Which columns are which?</h2>
-    </v-card-text>
-    <v-container class="pb-5">
-      <v-row>
-        <v-col>
-          <v-select
-            :items="columnItems"
-            :value="splitBasis"
-            label="Charge Current"
-            hint="I'll use this value's sign to sort out cycles, and discard
-            lines where it's ~zero. It doesn't have to be included in the output
-            data."
-            persistent-hint
-            @change="setSplitBasis"
-          />
+    <v-container class="mb-5">
+      <v-row
+        align="center"
+        class="text-center"
+      >
+        <v-col cols="2">
+          <v-btn
+            nuxt
+            color="grey darken-2"
+            to="/"
+          >
+            Back
+          </v-btn>
         </v-col>
-        <v-col>
-          <v-select
-            :items="keptColumnItems"
-            :value="voltageColumn"
-            label="Voltage"
-            @change="setVoltageColumn"
-          />
+
+        <v-col class="text-center">
+          <v-btn
+            class="mono body-2 nocap"
+            elevation="12"
+            outlined
+            nuxt
+            to="/"
+            large
+          >
+            {{ file.name }}
+          </v-btn>
         </v-col>
-        <v-col>
-          <v-select
-            :items="keptColumnItems"
-            :value="spcColumn"
-            label="Specific Capacity"
-            @change="setSpcColumn"
-          />
+
+        <v-col cols="2">
+          <v-btn
+            :loading="loading"
+            color="orange"
+            @click="handleNext"
+          >
+            Next
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
-    <v-card-text>
-      <h2 class="mt-5 text-center">Select columns to keep:</h2>
-      <p class="text-center">All data will be returned as absolute values.</p>
-      <v-container>
-        <v-row no-gutters>
-          <v-col v-for="(column, index) in columns" :key="index" cols="12" md="3" sm="4">
-            <v-checkbox v-model="handleToggle" :value="index" :label="column" color="orange" />
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card-text>
+
+    <v-container>
+      <v-expansion-panels
+        multiple
+        focusable
+        inset
+      >
+        <v-expansion-panel>
+          <v-expansion-panel-header class="title">
+            Which columns are which?
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-select
+                    :items="columnItems"
+                    :value="splitBasis"
+                    label="Charge Current"
+                    hint="I'll use this value's sign to sort out cycles, and discard
+          lines where it's ~zero. It doesn't have to be included in the output
+          data."
+                    persistent-hint
+                    @change="setSplitBasis"
+                  />
+                </v-col>
+                <v-col>
+                  <v-select
+                    :items="keptColumnItems"
+                    :value="voltageColumn"
+                    label="Voltage"
+                    @change="setVoltageColumn"
+                  />
+                </v-col>
+                <v-col>
+                  <v-select
+                    :items="keptColumnItems"
+                    :value="spcColumn"
+                    label="Specific Capacity"
+                    @change="setSpcColumn"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header class="title">
+            Which columns do you want to keep?
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-container>
+              <v-row no-gutters>
+                <v-col
+                  v-for="(column, index) in columns"
+                  :key="index"
+                  cols="12"
+                  md="3"
+                  sm="4"
+                >
+                  <v-checkbox
+                    v-model="handleToggle"
+                    :value="index"
+                    :label="column"
+                    color="orange"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+
+        <v-expansion-panel>
+          <v-expansion-panel-header class="title">
+            Would you like to modify them?
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-row
+              v-for="item in keptColumnItems"
+              :key="item.value"
+              align="center"
+            >
+              <v-col class="text-right">
+                {{ item.text }}:
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="Label"
+                  hint="Customize the name"
+                />
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="Multiply By"
+                  hint="Unit Conversion"
+                />
+              </v-col>
+              <v-col>
+                <v-text-field
+                  label="Round to"
+                  hint="Decimal Places"
+                />
+              </v-col>
+              <v-col>
+                <v-checkbox
+                  label="Absolute Value?"
+                />
+              </v-col>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
   </v-card>
 </template>
 
