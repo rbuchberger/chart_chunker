1e3b85291b74af0c67fef3b78b43ec3ed3abadc2
Robert Buchberger <robert@buchberger.cc>
2019-11-14 14:14:36 +0100

    Remove width compression from data table

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,42 +1,34 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-spacer />
-      <v-col sm="8">
-        <v-simple-table
-          dense
-          fixed-header
-          height="500"
-          class="datatable"
+  <v-simple-table
+    dense
+    fixed-header
+    height="500"
+    class="datatable"
+  >
+    <thead>
+      <tr>
+        <th
+          v-for="header in dataObject.headers"
+          :key="header"
         >
-          <thead>
-            <tr>
-              <th
-                v-for="header in dataObject.headers"
-                :key="header"
-              >
-                {{ header }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="(line, lineIndex) in dataObject.lines"
-              :key="lineIndex"
-            >
-              <td
-                v-for="(entry, entryIndex) in line"
-                :key="lineIndex.toString() + entryIndex.toString()"
-              >
-                {{ entry }}
-              </td>
-            </tr>
-          </tbody>
-        </v-simple-table>
-      </v-col>
-      <v-spacer />
-    </v-row>
-  </v-container>
+          {{ header }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(line, lineIndex) in dataObject.lines"
+        :key="lineIndex"
+      >
+        <td
+          v-for="(entry, entryIndex) in line"
+          :key="lineIndex.toString() + entryIndex.toString()"
+        >
+          {{ entry }}
+        </td>
+      </tr>
+    </tbody>
+  </v-simple-table>
 </template>
 <script>
 export default {
