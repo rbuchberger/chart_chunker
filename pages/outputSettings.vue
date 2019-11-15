0fc8af93a89cba4c242c4f3ee74bf584cb65fb2f
Robert Buchberger <robert@buchberger.cc>
2019-11-15 16:46:14 +0100

    Hide nonfunctional column mod settings

--- pages/outputSettings.vue
+++ pages/outputSettings.vue
@@ -15,7 +15,7 @@
           </v-btn>
         </v-col>
 
-        <v-col class="text-center">
+        <v-col class="text-center text-truncate">
           <v-btn
             class="mono body-2 nocap"
             elevation="12"
@@ -42,8 +42,9 @@
 
     <v-container>
       <v-expansion-panels
-        multiple
+        v-model="openPanel"
         focusable
+        multiple
         inset
       >
         <v-expansion-panel>
@@ -54,14 +55,19 @@
           <v-expansion-panel-content>
             <v-container>
               <v-row>
+                <p>
+                  Cycles are chunked by looking at charge current (first item
+                  below). When it changes sign, a new cycle half is created.
+                  When it is ~zero, that line will be discarded.
+                </p>
+              </v-row>
+              <v-row>
                 <v-col>
                   <v-select
                     :items="columnItems"
                     :value="splitBasis"
                     label="Charge Current"
-                    hint="I'll use this value's sign to sort out cycles, and discard
-          lines where it's ~zero. It doesn't have to be included in the output
-          data."
+                    hint="Not required in kept columns."
                     persistent-hint
                     @change="setSplitBasis"
                   />
@@ -71,6 +77,8 @@
                     :items="keptColumnItems"
                     :value="voltageColumn"
                     label="Voltage"
+                    hint="Must be a kept column (below)"
+                    persistent-hint
                     @change="setVoltageColumn"
                   />
                 </v-col>
@@ -79,6 +87,8 @@
                     :items="keptColumnItems"
                     :value="spcColumn"
                     label="Specific Capacity"
+                    hint="Must be a kept column (below)"
+                    persistent-hint
                     @change="setSpcColumn"
                   />
                 </v-col>
@@ -93,6 +103,12 @@
 
           <v-expansion-panel-content>
             <v-container>
+              <v-row>
+                <p>
+                  Columns selected here will be included in the output data; all
+                  others will be discarded.
+                </p>
+              </v-row>
               <v-row no-gutters>
                 <v-col
                   v-for="(column, index) in columns"
@@ -113,46 +129,67 @@
           </v-expansion-panel-content>
         </v-expansion-panel>
 
-        <v-expansion-panel>
+        <!-- <v-expansion-panel>
           <v-expansion-panel-header class="title">
             Would you like to modify them?
           </v-expansion-panel-header>
 
           <v-expansion-panel-content>
-            <v-row
-              v-for="item in keptColumnItems"
-              :key="item.value"
-              align="center"
-            >
-              <v-col class="text-right">
-                {{ item.text }}:
-              </v-col>
-              <v-col>
-                <v-text-field
-                  label="Label"
-                  hint="Customize the name"
-                />
-              </v-col>
-              <v-col>
-                <v-text-field
-                  label="Multiply By"
-                  hint="Unit Conversion"
-                />
-              </v-col>
-              <v-col>
-                <v-text-field
-                  label="Round to"
-                  hint="Decimal Places"
-                />
-              </v-col>
-              <v-col>
-                <v-checkbox
-                  label="Absolute Value?"
-                />
-              </v-col>
-            </v-row>
+            <v-container>
+              <v-row>
+                <p>
+                  These settings allow you to modify the values in a given
+                  column. You can modify the base header, multiply all entries
+                  by some value (useful for unit conversions), round them to a
+                  particular number of decimal places, or take their absolute
+                  value.
+                </p>
+              </v-row>
+              <v-container
+                v-for="column in keptColumnItems"
+                :key="column.value"
+                class="my-4"
+              >
+                <h4 class="subtitle-1">
+                  {{ column.text }}:
+                </h4>
+                <v-row align="center">
+                  <v-col cols="5">
+                    <v-text-field
+                      :value="column.labelMod"
+                      label="Label"
+                      data-property="labelMod"
+                      data-columnIndex="column.value"
+                      hint="Customize the name"
+                      @change="modColumn"
+                    />
+                  </v-col>
+                  <v-col>
+                    <v-text-field
+                      :value="column.multiply"
+                      label="Multiply By"
+                      hint="Unit Conversion"
+                    />
+                  </v-col>
+                  <v-col>
+                    <v-text-field
+                      :value="column.round"
+                      label="Round to"
+                      hint="Decimal Places"
+                    />
+                  </v-col>
+                  <v-col>
+                    <v-checkbox
+                      :value="column.abs"
+                      label="Absolute Value"
+                      color="primary"
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-container>
           </v-expansion-panel-content>
-        </v-expansion-panel>
+        </v-expansion-panel> -->
       </v-expansion-panels>
     </v-container>
   </v-card>
@@ -171,15 +208,21 @@
 import { mapGetters, mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
+  data() {
+    return {
+      openPanel: [0]
+    }
+  },
+
   computed: {
-    ...mapGetters([
+    ...mapGetters(['columns', 'keptColumnItems', 'spcColumn', 'voltageColumn']),
+    ...mapState([
+      'splitBasis',
+      'keptColumns',
       'columnItems',
-      'columns',
-      'keptColumnItems',
-      'spcColumn',
-      'voltageColumn'
+      'loading',
+      'file'
     ]),
-    ...mapState(['splitBasis', 'keptColumns', 'loading', 'file']),
     handleToggle: {
       get() {
         return this.keptColumns
@@ -196,7 +239,12 @@ export default {
       'setKeptColumns',
       'setSpcColumn',
       'setVoltageColumn'
+      // 'modColumn'
     ]),
+
+    modColumn(event) {
+      console.log(event)
+    },
 
     ...mapActions(['buildChunker']),
     handleNext() {
