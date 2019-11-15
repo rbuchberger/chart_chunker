3950ec52eaa3f606fd26a6b2f41a4b2722647a8d
Robert Buchberger <robert@buchberger.cc>
2019-11-15 16:06:21 +0100

    Improve all cycle page layout

--- components/AllCycles.vue
+++ components/AllCycles.vue
@@ -3,62 +3,8 @@
     <v-row>
       <v-col>
         <h4 class="display-1 text-center mb-5">
-          {{ cycleCount }} Cycle Analysis
+          {{ cycleCount }} Cycles
         </h4>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col>
-        <h4 class="subtitle-1 text-center">
-          Charge Efficiency (%):
-        </h4>
-        <div class="d-flex">
-          <div class="flex-grow-0 d-flex flex-column justify-space-around">
-            <div>
-              {{ Math.max(...chunker.chargeEffArray) }}
-            </div>
-            <div class="flex-grow-1" />
-            <div>
-              {{ Math.min(...chunker.chargeEffArray) }}
-            </div>
-          </div>
-          <v-sparkline
-            class="flex-grow-1"
-            :value="chargeEfficiencyGraphData"
-            color="orange"
-            smooth
-            auto-draw
-            line-width="1"
-            height="120"
-          />
-        </div>
-      </v-col>
-
-      <v-col>
-        <h4 class="subtitle-1 text-center">
-          Retention (%):
-        </h4>
-        <div class="d-flex">
-          <div class="flex-grow-0 d-flex flex-column justify-space-around">
-            <div>
-              {{ Math.max(...chunker.retentionArray) }}
-            </div>
-            <div class="flex-grow-1" />
-            <div>
-              {{ Math.min(...chunker.retentionArray) }}
-            </div>
-          </div>
-          <v-sparkline
-            :value="retentionGraphData"
-            class="flex-grow-1"
-            color="orange"
-            smooth
-            auto-draw
-            line-width="1"
-            height="120"
-          />
-        </div>
       </v-col>
     </v-row>
 
@@ -68,27 +14,101 @@
         cols="4"
         class="text-right"
       >
-        <v-btn @click="copySummary">
-          <v-icon
-            left
-            color="orange"
-          >
-            mdi-clipboard-outline
-          </v-icon>
-          Copy This Analysis
-        </v-btn>
+        <v-tooltip bottom>
+          <template #activator="{ on }">
+            <v-btn
+              @click="copySummary"
+              v-on="on"
+            >
+              <v-icon
+                left
+                color="orange"
+              >
+                mdi-clipboard-outline
+              </v-icon>
+              Copy Analysis
+            </v-btn>
+          </template>
+          Copy the table below <v-icon>mdi-arrow-down</v-icon>
+        </v-tooltip>
       </v-col>
       <v-col cols="4">
-        <v-btn @click="copyAll">
-          <v-icon
-            left
-            color="orange"
-          >
-            mdi-clipboard-outline
-          </v-icon>Copy All Data
-        </v-btn>
+        <v-tooltip bottom>
+          <template #activator="{ on }">
+            <v-btn
+              @click="copyAll"
+              v-on="on"
+            >
+              <v-icon
+                left
+                color="orange"
+              >
+                mdi-clipboard-outline
+              </v-icon>Copy All Data
+            </v-btn>
+          </template>
+          Copy the formatted data from all cycles, side-by-side.
+        </v-tooltip>
       </v-col>
       <v-spacer />
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-card outlined>
+          <v-card-title class="subtitle-1">
+            Charge Efficiency (%):
+          </v-card-title>
+          <v-card-text class="d-flex">
+            <div class="flex-grow-0 d-flex flex-column justify-space-around">
+              <div>
+                {{ Math.max(...chunker.chargeEffArray) }}
+              </div>
+              <div class="flex-grow-1" />
+              <div>
+                {{ Math.min(...chunker.chargeEffArray) }}
+              </div>
+            </div>
+            <v-sparkline
+              class="flex-grow-1"
+              :value="chargeEfficiencyGraphData"
+              color="orange"
+              smooth
+              auto-draw
+              line-width="1"
+              height="120"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col>
+        <v-card outlined>
+          <v-card-title class="subtitle-1">
+            Retention (%):
+          </v-card-title>
+          <v-card-text class="d-flex">
+            <div class="flex-grow-0 d-flex flex-column justify-space-around">
+              <div>
+                {{ Math.max(...chunker.retentionArray) }}
+              </div>
+              <div class="flex-grow-1" />
+              <div>
+                {{ Math.min(...chunker.retentionArray) }}
+              </div>
+            </div>
+            <v-sparkline
+              :value="retentionGraphData"
+              class="flex-grow-1"
+              color="orange"
+              smooth
+              auto-draw
+              line-width="1"
+              height="120"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
 
     <DataTable :data-object="overviewTableData" />
